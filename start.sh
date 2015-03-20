d63b959f21f2b7ca15a3487584a891bc126ad56c
Kundan Kadam <kkadam1@tigers.lsu.edu>
2015-03-20 16:36:13 -0500

    bug fixes, readme file in run dir

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 ### Set important parameters ###
-run=test_automation
-resolution=LR  
+run=m8
+readme="q0.5 benchmark at quadruple resolution!"
+resolution=SHR  
 #Options for resolution are LR, HR, SHR
 work_dir=/work/kkadam/scf_runs
 greens=".true."
@@ -23,7 +24,7 @@ then
    tm=tmr_array_130x130x256
    sm=smz_array_130x130x256
 
-elif [ $resolution -eq "HR" ] 
+elif [ $resolution == "HR" ] 
 then
    sed -i -e '1,3d' runscf.h
    sed -i "1i\    integer, parameter :: numr = 258" runscf.h
@@ -32,7 +33,7 @@ then
    tm=tmr_array_258x258x512
    sm=smz_array_258x258x512
 
-elif [ $resolution -eq "SHR" ] 
+elif [ $resolution == "SHR" ] 
 then
    sed -i -e '1,3d' runscf.h
    sed -i "1i\    integer, parameter :: numr = 258" runscf.h
@@ -41,7 +42,9 @@ then
    tm=tmr_array_514x514x1024
    sm=smz_array_514x514x1024
 else
+   echo "================================================="
    echo "Wrong resolution! Allowed values are LR, HR, SHR."
+   echo "================================================="
    exit
 fi
 
@@ -56,7 +59,7 @@ make
 if [ $? -eq 0 ]
 then  
    echo "========================================================"
-   echo " Binary scf successfully compiled!"
+   echo " Binary file "scf" successfully compiled!"
    echo "========================================================"
 else
    echo "========================================================"
@@ -75,7 +78,10 @@ mkdir $run
 cp scf $run
 cp init $run
 cp runscf.h $run
+echo $readme > $run/readme
 
+sed -i -e '8d' bs
+sed -i "8i\#PBS -N $run" bs
 sed -i -e '11d' bs
 sed -i "11i\\$work_dir\/$run\/scf" bs
 
@@ -92,6 +98,7 @@ if [ -d $work_dir\/$run ]; then
 else
    mv $run $work_dir
    if [ $greens == ".true." ]; then 
+      echo "Copying tmr_array and smz_array..."
       cp "$greens_repo/$tm" "$work_dir/$run/tmr_array"
       cp "$greens_repo/$sm" "$work_dir/$run/smz_array"
    fi
